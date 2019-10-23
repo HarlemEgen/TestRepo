a82f89dc4ab360b2231a095065cacf22b3ea3fca
Harlem <1017058@student.sae.edu.au>
2019-10-23 16:12:58 +1100

    Deleted other Project, made new one (Turret and Minion)

---
+++ Assets/Scripts/HE_Turret.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates towards the mouse position and shoots when you click
+/// </summary>
+public class HE_Turret : MonoBehaviour
+{
+    void Update()
+    {
+
+        //ROTATES TO MOUSE POSITION
+        //THE INFORMATION OF WHAT OUR RAYCAST HAS HIT
+        RaycastHit hit;
+        //CREATE A RAYCAST TO POINT AT THE MOUSE'S POSITION.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //IF OUR RAYCAST HITS AN OBJECT
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            //transform.LookAt(hit.transform.position);
+            transform.LookAt(new Vector3(hit.point.x, 0, hit.point.z));
+        }
+        //SHOOTS WHEN MOUSE BUTTON IS CLICKED
+
+    }
+}
